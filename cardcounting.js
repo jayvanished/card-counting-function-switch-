25daeff6d0b1662ae0e017021799d1543971f443
jayvanished <52804212+jayvanished@users.noreply.github.com>
2020-02-20 05:51:43 +0100

    Create cardcounting.js

---
+++ cardcounting.js
@@ -0,0 +1,26 @@
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count += 1;
+    break;
+  case 7:
+  case 8:
+  case 9:
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+    count -=1;
+    break;
+}
+return (count > 0) ? count + " Bet"
+  : count + " Hold"
+}
